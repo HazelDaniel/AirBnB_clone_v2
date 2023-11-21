178b42194e0ef1cf0edf5cb7e6ae9612d2b6c3ec
HazelDaniel <hazeldaniel856@gmail.com>
2023-11-21 22:41:28 +0100

    task 6: created a declarative base class and made other classes inherit from both it and the basemodel class. basemodel is like an abstract class to the orm scope. added a switching logic between the storage types and created the db storage class

---
+++ models/engine/db_storage.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python3
+from models import User, City, Place, State, Review, Amenity
+from models.base_model import Base
+from os import getenv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, scoped_session
+
+
+name_to_class_mapper = {"User": User, "City": City, "Place": Place,
+                        "State": State, "Review": Review, "Amenity": Amenity}
+
+dialect, driver = "mysql", "mysqldb"
+user, password = getenv("HBNB_MYSQL_USER"), getenv("HBNB_MYSQL_PWD")
+host, db_url = getenv("HBNB_MYSQL_HOST"), getenv("HBNB_MYSQL_DB")
+hbnb_env = getenv("HBNB_ENV")
+
+metadata = None
+
+
+class DBStorage:
+    """a class implementation of the database storage"""
+    __engine = None
+    __session = None
+
+    def __init__(self):
+        """a constructor function for the DBStorage class"""
+        connection_url = f"{dialect}+{driver}://{user}:"
+        f"{password}@{host}/{db_url}"
+        DBStorage.__engine = create_engine(connection_url, pool_pre_ping=True)
+        global metadata
+        metadata = Base.metadata
+        if (hbnb_env == "test"):
+            metadata.drop_all()
+
+    def all(self, cls=None):
+        """ this querys the db session and returns
+            a dict of all objects in the query """
+        if not DBStorage.__session:
+            return {}
+        if not cls:
+            users = DBStorage.__session.query(User).all()
+            cities = DBStorage.__session.query(City).all()
+            states = DBStorage.__session.query(State).all()
+            reviews = DBStorage.__session.query(Review).all()
+            amenities = DBStorage.__session.query(Amenity).all()
+            res = [*amenities, *cities, *reviews, *states, *users]
+            res_dict = {f"{entry.__dict__['__class__']}.{entry.id}": entry
+                        for entry in res}
+            return res_dict
+        else:
+            if cls not in name_to_class_mapper:
+                return []
+            res = DBStorage.__session.query(name_to_class_mapper[cls]).all()
+            res_dict = {f"{entry.__dict__['__class__']}.{entry.id}": entry
+                        for entry in res}
+            return res_dict
+
+    def new(self, obj):
+        """adds the object to the current database session"""
+        self.__session.add(obj)
+
+    def save(self):
+        """commits all changes of the current database session"""
+        self.__session.commit()
+
+    def delete(self, obj):
+        """delete from the current database session obj if not None"""
+        if not obj:
+            return
+        self.__session.delete(obj)
+
+    def reload(self):
+        """creates all tables in the database"""
+        if metadata:
+            metadata.create_all(bind=self.__engine)
+        self.__session = scoped_session(sessionmaker(bind=self.__engine,
+                                                     expire_on_commit=False))()
--- models/state.py
+++ models/state.py
@@ -1,8 +1,24 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from os import getenv
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+from . import storage
+
+storage_type = getenv("HBNB_TYPE_STORAGE")
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    if storage_type == "db":
+        cities = relationship("City", backref="state")
+
+    @property
+    def cities(self):
+        if (storage_type == "file"):
+            state_id_mapper = lambda x: x.state_id == self.id
+            return map(state_id_mapper, storage.all().values())
+        return None
