02c2a83d14d99a71109a23847ef6fb6cd45c2cbb
HazelDaniel <hazeldaniel856@gmail.com>
2023-12-06 21:58:14 +0100

    task-0: submission

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+# this script Sets up a web server for deployment of web_static.
+
+sudo su root -
+sudo apt-get update
+sudo apt-get install nginx
+sudo mkdir /data
+sudo mkdir -p /data/web_static
+sudo mkdir -p /data/web_static/releases
+sudo mkdir -p /data/web_static/shared
+sudo mkdir -p /data/web_static/releases/test
+sudo touch /data/web_static/releases/test/index.html
+echo "this is the test page" > /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data/
+
+printf %s "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    add_header X-Served-By $HOSTNAME;
+    root   /var/www/html;
+    index  index.html index.htm;
+
+    location /hbnb_static {
+        alias /data/web_static/current;
+        index index.html index.htm;
+    }
+
+    location /redirect_me {
+        return 301 http://cuberule.com/;
+    }
+
+    error_page 404 /404.html;
+    location /404 {
+      root /var/www/html;
+      internal;
+    }
+}" > /etc/nginx/sites-available/default
+
+service nginx restart
