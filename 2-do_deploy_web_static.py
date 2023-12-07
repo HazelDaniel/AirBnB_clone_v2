73d8f31e8d2d6e068960107b0053d994cd904397
HazelDaniel <hazeldaniel856@gmail.com>
2023-12-07 22:27:40 +0100

    task-2: re-submission

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -5,24 +5,28 @@ import os.path
 
 env.user = 'ubuntu'
 env.hosts = ["54.197.123.187", "18.210.17.238"]
-env.key_filename = "~/id_rsa"
+env.key_filename = "~/Documents/ALX/credentials/alx_ubuntu_server"
 
 
 def do_deploy(archive_path):
     """ this distributes an archive to my web servers """
     if not os.path.exists(archive_path):
+        print(f"{archive_path}: does not exist")
         return False
     try:
         arc = archive_path.split("/")
         base = arc[1].strip('.tgz')
-        put(archive_path, '/tmp/')
+        put(archive_path, remote_path='/tmp/')
+        print("we got here")
+        print(f"mkdir -p /data/web_static/releases/{base}")
         sudo(f"mkdir -p /data/web_static/releases/{base}")
         main = f"/data/web_static/releases/{base}"
+        print(f"tar -xzf /tmp/{arc[1]} -C {main}/")
         sudo(f"tar -xzf /tmp/{arc[1]} -C {main}/")
         sudo(f"rm /tmp/{arc[1]}")
         sudo(f"mv {main}/web_static/* {main}/")
         sudo("rm -rf /data/web_static/current")
-        sudo(f"ln -s {main}/ \"/data/web_static/current\"")
+        sudo(f"ln -sf {main}/ /data/web_static/current")
         return True
-    except:
+    except Exception:
         return False
