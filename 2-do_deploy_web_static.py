435ddbc46f26a17fa363114e047c0180c57198be
HazelDaniel <hazeldaniel856@gmail.com>
2023-12-07 18:35:45 +0100

    task-2: submission

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -4,7 +4,7 @@ from fabric.api import *
 import os.path
 
 env.user = 'ubuntu'
-env.hosts = ["104.196.155.240", "34.74.146.120"]
+env.hosts = ["54.197.123.187", "18.210.17.238"]
 env.key_filename = "~/id_rsa"
 
 
@@ -18,8 +18,8 @@ def do_deploy(archive_path):
         put(archive_path, '/tmp/')
         sudo(f"mkdir -p /data/web_static/releases/{base}")
         main = f"/data/web_static/releases/{base}"
-        sudo("tar -xzf /tmp/{arc[1]} -C {main}/")
-        sudo(l"rm /tmp/{arc[1]}")
+        sudo(f"tar -xzf /tmp/{arc[1]} -C {main}/")
+        sudo(f"rm /tmp/{arc[1]}")
         sudo(f"mv {main}/web_static/* {main}/")
         sudo("rm -rf /data/web_static/current")
         sudo(f"ln -s {main}/ \"/data/web_static/current\"")
