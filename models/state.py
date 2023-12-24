4f365dee279b64ae8a977d9aefbd8c4db7b8a24c
HazelDaniel <hazeldaniel856@gmail.com>
2023-12-25 00:04:20 +0100

    task-7: re-submission. upgraded the file storage to allow passing both string arguments and classes as parameter in the all method

--- models/state.py
+++ models/state.py
@@ -9,29 +9,16 @@ from . import storage
 storage_type = getenv("HBNB_TYPE_STORAGE")
 
 
-# if storage_type == "db":
-#     class State(BaseModel, Base):
-#         """ the class implementation of states table """
-#         __tablename__ = "states"
-#         if storage_type == "db":
-#             name = Column(String(128), nullable=False)
-#             cities = relationship("City", backref="state")
-# else:
-#     class State(BaseModel):
-#         """ the class implementation of the states storage"""
-#         @property
-#         def cities(self):
-#             """returns the list of City instances
-#                 with state_id equals to the current State.id"""
-#             return map(lambda x: x.state_id == self.id,
-#                        storage.all("City").values())
-class State(BaseModel, Base):
-    """ the class implementation of the states storage"""
-    __tablename__ = "states"
-    if storage_type == "db":
-        name = Column(String(128), nullable=False)
-        cities = relationship("City", backref="state")
-    else:
+if storage_type == "db":
+    class State(BaseModel, Base):
+        """ the class implementation of states table """
+        __tablename__ = "states"
+        if storage_type == "db":
+            name = Column(String(128), nullable=False)
+            cities = relationship("City", backref="state")
+else:
+    class State(BaseModel):
+        """ the class implementation of the states storage"""
         @property
         def cities(self):
             """returns the list of City instances
