7d4094b22525c8d6bf6de4d2df07359ac87f9be7
HazelDaniel <hazeldaniel856@gmail.com>
2023-12-24 12:22:08 +0100

    task-0: re-submission. properly documented the code

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# a module that starts a flask web application on 0.0.0.0 port 5000
+"""a module that starts a flask web application on 0.0.0.0 port 5000"""
 from flask import Flask
 
 app = Flask(__name__)
