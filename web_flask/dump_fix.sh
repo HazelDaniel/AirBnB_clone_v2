813529c181d3eaa87e88d1dbe074e8ff27c7b076
HazelDaniel <hazeldaniel856@gmail.com>
2023-12-27 17:31:27 +0100

    task-8: submission

--- web_flask/dump_fix.sh
+++ web_flask/dump_fix.sh
@@ -2,8 +2,8 @@
 # this bash script fixes the sql dump issue pertaining to foreign key constraint failure
 
 dump_path="$(dirname ${BASH_SOURCE[0]})/7-dump.sql"
-if cat "$dump_path" | grep -Pi "password\('hbnb_dev_pwd'\)"; then
-	echo "";
+if cat "$dump_path" | grep -Piq "password\('hbnb_dev_pwd'\)"; then
+	echo "" 1> /dev/null;
 else
 	sed -i "s/'hbnb_dev_pwd'/PASSWORD('hbnb_dev_pwd')/g""$dump_path"
 fi
