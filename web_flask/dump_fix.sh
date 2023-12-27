e9870be28fc30a9f6fd14685d2305216664943a2
HazelDaniel <hazeldaniel856@gmail.com>
2023-12-27 17:07:32 +0100

    fixed the bash script meant to fix the sql dump on the server

--- web_flask/dump_fix.sh
+++ web_flask/dump_fix.sh
@@ -2,4 +2,4 @@
 # this bash script fixes the sql dump issue pertaining to foreign key constraint failure
 
 dump_path="$(dirname ${BASH_SOURCE[0]})/7-dump.sql"
-sed -i "s/'hbnb_dev_pwd'/PASSWORD('hbnb_dev_pwd)/g" "$dump_path" ; sed -i "s/) ENGINE=InnoDB.*/) ENGINE=InnoDB;/g" "$dump_path"
+sed -i "s/'hbnb_dev_pwd'/PASSWORD('hbnb_dev_pwd')/g" "$dump_path" ; sed -i "s/) ENGINE=InnoDB.*/) ENGINE=InnoDB;/g" "$dump_path"
